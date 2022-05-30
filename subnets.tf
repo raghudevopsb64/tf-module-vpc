232d6f4e5b59bb14349dd8a09b44e3e4145a21b6
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-30 08:10:00 +0530

    Update roboshop-infra/tf-module-vpc/

---
+++ subnets.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "main" {
+  count             = length(var.SUBNET_CIDR)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.SUBNET_CIDR, count.index)
+  availability_zone = element(var.AZ, count.index)
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}-${count.index}"
+  }
+}
+
