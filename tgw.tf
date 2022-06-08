24eabd1d195aeb23b7bdc750a5f4f23964ba94e3
Raghu K <raghuk.vit@gmail.com>
2022-06-08 08:38:28 +0530

    Update roboshop-infra/all-modules/tf-module-vpc/

--- tgw.tf
+++ tgw.tf
@@ -9,3 +9,22 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "component-attach" {
   }
 }
 
+
+resource "aws_ec2_transit_gateway_route_table" "app-vpc" {
+  transit_gateway_id = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
+  tags = {
+    Name = "frontend-vpc"
+  }
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "default-vpc-tgw-attach" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.component-attach.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.app-vpc.id
+}
+
+resource "aws_ec2_transit_gateway_route" "route-to-app-vpc" {
+  destination_cidr_block         = "0.0.0.0/0"
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.component-attach.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.app-vpc.id
+}
+
