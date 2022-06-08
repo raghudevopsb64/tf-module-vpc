17f33c327c6aef30a9af04517cb56f5ce0b9fb2e
Raghu K <raghuk.vit@gmail.com>
2022-06-08 08:37:06 +0530

    Update roboshop-infra/all-modules/tf-module-vpc/

--- tgw.tf
+++ tgw.tf
@@ -1,7 +1,9 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "component-attach" {
-  subnet_ids         = aws_subnet.main.*.id
-  transit_gateway_id = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
-  vpc_id             = aws_vpc.main.id
+  subnet_ids                                      = aws_subnet.main.*.id
+  transit_gateway_id                              = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  vpc_id                                          = aws_vpc.main.id
   tags = {
     Name = "${var.COMPONENT}-tgw-attach-${var.ENV}"
   }
