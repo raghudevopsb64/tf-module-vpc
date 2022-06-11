c63dc0852b09183ad3c7c5e344f1d1ff827a595f
Raghu K <raghuk.vit@gmail.com>
2022-06-11 09:16:49 +0530

    Update roboshop-infra/all-modules/tf-module-vpc/

--- tgw.tf
+++ tgw.tf
@@ -10,14 +10,38 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "component-attach" {
 }
 
 
-resource "aws_ec2_transit_gateway_route_table_association" "component-vpc-tgw-attach" {
+resource "aws_ec2_transit_gateway_route_table" "app-vpc" {
+  transit_gateway_id = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
+  tags = {
+    Name = "${var.COMPONENT}-vpc"
+  }
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "default-vpc-tgw-attach" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.component-attach.id
-  transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.APP_VPC_TRANSIT_GW_ROUTE_TABLE
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.app-vpc.id
+}
+
+resource "aws_ec2_transit_gateway_route" "route-to-app-vpc" {
+  destination_cidr_block         = "0.0.0.0/0"
+  transit_gateway_attachment_id  = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_TRANSIT_GW_ATTACHMENT
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.app-vpc.id
 }
 
 resource "aws_ec2_transit_gateway_route" "route-from-default-vpc-to-component-vpc" {
   destination_cidr_block         = var.VPC_CIDR_BLOCK
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.component-attach.id
-  transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.APP_VPC_TRANSIT_GW_ROUTE_TABLE
+  transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_TRANSIT_GW_ROUTE_TABLE
 }
 
+
+//resource "aws_ec2_transit_gateway_route_table_association" "component-vpc-tgw-attach" {
+//  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.component-attach.id
+//  transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.APP_VPC_TRANSIT_GW_ROUTE_TABLE
+//}
+//
+//resource "aws_ec2_transit_gateway_route" "route-from-default-vpc-to-component-vpc" {
+//  destination_cidr_block         = var.VPC_CIDR_BLOCK
+//  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.component-attach.id
+//  transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.APP_VPC_TRANSIT_GW_ROUTE_TABLE
+//}
