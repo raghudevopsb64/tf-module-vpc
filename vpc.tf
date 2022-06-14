c7548cbd7a0530bae34344e21c459818a34bf24b
Raghu K <raghuk.vit@gmail.com>
2022-06-14 07:48:23 +0530

    Update roboshop-infra/all-modules/tf-module-vpc/

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.VPC_CIDR_BLOCK
+  cidr_block           = var.VPC_CIDR_BLOCK
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name      = "${var.COMPONENT}-${var.ENV}"
     COMPONENT = var.COMPONENT
